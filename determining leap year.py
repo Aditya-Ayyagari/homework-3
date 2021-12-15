a4b61cbd4c9d7f340478161721607fd90a0f0339
adityaayyagari <adityaayyagari09@gmail.com>
2021-12-16 04:47:06 +0530

    homework3

---
+++ determining leap year.py
@@ -0,0 +1,10 @@
+num1=int(input("enter any year: "))
+if num1%4==0 or num1%400==0:
+    print("year is leap year")
+else:
+    print("year is not leap year")
+
+
+
+
+
---
+++ gretest of five number.py
@@ -0,0 +1,16 @@
+n1=int(input("enter first nimber n1:"))
+n2=int(input("enter second number n2"))
+n3=int(input("enter third number n3:"))
+n4=int(input("enter forth number n4:"))
+n5=int(input("enter fifith number n5:"))
+if n1>n2 and n1>n3 and n1>n4 and n1>n5:
+    print("n1 is largest number among five")
+elif n2>n3 and n2>n4 and n2>n5:
+    print("n2 is largest number among five")
+elif n3>n4 and n3>n5:
+    print("n3 is largest number among five")
+elif n4>n5:
+    print("n4 is largest among five number1")
+else:
+    print("n5 is largest among five number")
+
